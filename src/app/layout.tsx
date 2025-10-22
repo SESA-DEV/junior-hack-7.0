162be1b33250753c0cae5ecf6669e6cfe0b1f507
Hasinthaka Piyumal <hasinthakapiyumal@gmail.com>
2025-10-22 18:27:51 +0530

    modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -111,6 +111,8 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="format-detection" content="telephone=no" />
+        <meta name="google-site-verification" content="WtQmgNW3CwWwlfP-vSVtfTRxXTLafQ539ZQANU9y2KM" />
+
         
         {/* Structured Data - Organization */}
         <script
